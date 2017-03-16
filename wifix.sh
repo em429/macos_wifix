0d74f80a9aaf5f9fb0fbfa6cc45205846e441994
hajzso <mate@lambd.ax>
2017-03-16 13:00:44 +0100

    first commit

---
+++ wifix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# fixes that sodding wifi when on fragile networks (macOS)
+
+sudo ifconfig en0 down
+sudo route flush &
+pid[0]=$!
+sleep 5
+trap "kill ${pid[0]}; exit 1" INT
+sudo ifconfig en0 up
